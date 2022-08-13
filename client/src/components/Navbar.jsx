15b4e9a4f3aa594de8d505ab51d76502b2c42d76
EthemCuhadar <iethemcuhadar@gmail.com>
2022-08-13 16:13:59 +0300

    Styled-components of Navbar created

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    height: 60px;
+`
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Container>
+        Navbar
+    </Container>
   )
 }
 
