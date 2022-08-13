44fb8415eb6607d9947987c02f9db29e7232991f
EthemCuhadar <iethemcuhadar@gmail.com>
2022-08-13 23:29:50 +0300

    Home page reorganized

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Announcements from '../components/Announcements'
 import Navbar from '../components/Navbar'
+import Slider from '../components/Slider'
 
 const Home = () => {
   return (
