e3095eaff6b7e48b08dae1b9530ef61273c1204c
EthemCuhadar <iethemcuhadar@gmail.com>
2022-08-13 16:55:34 +0300

    Left side and center organized

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import SearchIcon from '@mui/icons-material/Search';
 
 const Container = styled.div`
     height: 60px;
@@ -8,11 +9,31 @@ const Container = styled.div`
 const Wrapper = styled.div`
     padding: 10px 20px;
     display: flex;
+    align-items: center;
     justify-content: space-between; 
 `
 
 const Left = styled.div`
     flex: 1;
+    display: flex;
+    align-items: center;
+`
+
+const Language = styled.div`
+    font-size: 14px;
+    cursor: pointer;
+`
+
+const SearchContainer = styled.div`
+    border: 1px solid lightgray;
+    display: flex;
+    align-items: center;
+    margin-left: 25px;
+    padding: 5px;
+`
+
+const Input = styled.input`
+    border: none;
 `
 
 const Right = styled.div`
@@ -23,12 +44,26 @@ const Center = styled.div`
     flex: 1;
 `
 
+const Logo = styled.h1`
+    font-weight: bold;
+`
+
 const Navbar = () => {
   return (
     <Container>
         <Wrapper>
-            <Left>left</Left>
-            <Center>center</Center>
+            <Left>
+                <Language>
+                    EN
+                </Language>
+                <SearchContainer>
+                    <Input/>
+                    <SearchIcon />
+                </SearchContainer>
+            </Left>
+            <Center>
+                <Logo>LOGO.</Logo>
+            </Center>
             <Right>right</Right>
         </Wrapper>
     </Container>
