0771b88e1162209017842b6a8ba231929afdabba
EthemCuhadar <iethemcuhadar@gmail.com>
2022-08-13 16:24:35 +0300

    Left, center and right parts of the wrapper created

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -9,11 +9,17 @@ const Wrapper = styled.div`
     padding: 10px 20px;
 `
 
+const Left = styled.div``
+const Right = styled.div``
+const Center = styled.div``
+
 const Navbar = () => {
   return (
     <Container>
         <Wrapper>
-            Navbar
+            <Left>left</Left>
+            <Center>center</Center>
+            <Right>right</Right>
         </Wrapper>
     </Container>
   )
